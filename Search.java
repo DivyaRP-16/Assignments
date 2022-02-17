df5980f5414729b23ea30b10df719b8016315d46
Divya Raj P <98905712+DivyaRP-16@users.noreply.github.com>
2022-02-17 23:38:32 +0530

    Add files via upload

---
+++ Search.java
@@ -0,0 +1,14 @@
+public class Search
+{     import java.util.Arrays;
+import java.util.Scanner;
+
+    public class Search {
+        public static void main(String[] ar){
+            int a[] = {5, 12, 14, 6, 78, 19, 1, 23, 26, 35, 37, 7, 52, 86, 47};
+            int search_ele=a[];
+            for(int i=0;i<a.length-1;i++)
+            {
+                System.out.println(a[i]);
+            }
+        }
+}
