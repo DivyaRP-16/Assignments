a05ab8f82f502b8416e455c01b3bd1d213503c88
Divya Raj P <98905712+DivyaRP-16@users.noreply.github.com>
2022-02-28 20:33:29 +0530

    q1

---
+++ Java11Features/java11Assignment.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import static javax.swing.text.html.HTML.Tag.P;
+
+public class java11Assignment {
+    @FunctionalInterface
+    public interface SimpleInterest{
+        public void simple(int P,int R,int T);
+
+    }
+
+    public static void main(String[] args) {
+        SimpleInterest si=(var P,var R,var T)-> System.out.println(P*R*T/100);
+        si.simple(112,10,2);
+
+    }
+}
